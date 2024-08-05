4cafab67e6a40133162914eef6eba3789aa17a38
eeeseohyun <eeeseohyun@gmail.com>
2024-08-05 23:45:59 +0900

    token으로 id 찾기

--- todaySpoon/src/main/java/com/example/todaySpoon/Controller/MemberController.java
+++ todaySpoon/src/main/java/com/example/todaySpoon/Controller/MemberController.java
@@ -4,6 +4,7 @@ package com.example.todaySpoon.controller;
 import com.example.todaySpoon.Dto.MemberDto;
 import com.example.todaySpoon.Dto.SignInDto;
 import com.example.todaySpoon.Dto.SignUpDto;
+import com.example.todaySpoon.auth.jwt.JwtProvider;
 import com.example.todaySpoon.auth.jwt.JwtToken;
 import com.example.todaySpoon.Service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,10 +12,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -24,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final UserService userService;
-
+    private final JwtProvider jwtProvider;
     @Operation(summary = "로그인", description="id와 password로 로그인하고 토큰 반환합니다")
     @PostMapping("/login")
     public JwtToken signIn(@RequestBody SignInDto signInDto) {
@@ -40,5 +39,13 @@ public class MemberController {
     public ResponseEntity<MemberDto> signUp(@RequestBody SignUpDto signUpDto) {
         MemberDto savedMemberDto = userService.signUp(signUpDto);
         return ResponseEntity.ok(savedMemberDto);
+    }
+
+    @GetMapping("/token-userid")
+    public ResponseEntity<?> getUserInfo(@RequestHeader("Authorization") String authorizationHeader) {
+        String token = authorizationHeader.replace("Bearer ", "");
+        Authentication authentication = jwtProvider.getAuthentication(token);
+        // 사용자 정보를 반환합니다. 여기서는 단순히 사용자 이름만 반환합니다.
+        return ResponseEntity.ok(authentication.getName());
     }
 }
